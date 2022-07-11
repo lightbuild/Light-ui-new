5b58cabe43902ff808a0fb53a41c801d83938d3c
lightbuild <renran6303@gmail.com>
2022-07-11 14:46:35 +0800

    aside样式初步更新

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -37,5 +37,21 @@
 </script>
 
 <style lang="scss" scoped>
-
+  aside {
+    background: lightblue;
+    width: 150px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: 70px 16px 16px;
+  
+    > h2 {
+      margin-bottom: 4px;
+    }
+    > ol {
+      > li {
+        padding: 4px 0;
+      }
+    }
+  }
 </style>
