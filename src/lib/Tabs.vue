8316af4d3f93b9b461ab2dcdd025844992ed04d3
lightbuild <renran6303@gmail.com>
2022-07-15 17:46:32 +0800

    渲染Tabs内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    Tabs 组件
+    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+    <component v-for="(c,index) in defaults" :key="index" :is="c"/>
   </div>
 </template>
+
+<script setup lang="ts">
+  import Tab from "./Tab.vue"
+  import {useSlots} from 'vue';
+  
+  const slots = useSlots()
+  const defaults = slots.default()
+  defaults.forEach(tab =>{
+    if(tab.type !== Tab){
+      throw new Error('Tabs 的子标签必须是 Tab组件')
+    }
+  })
+  const titles = defaults.map(tab =>{
+    return tab.props.title
+  })
+</script>
