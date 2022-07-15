9a12323f55e1603ff7a5ce70723331934f14a908
lightbuild <renran6303@gmail.com>
2022-07-15 15:26:29 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    我是Dialog组件
-  </div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
 <script setup lang="ts">
+  import Dialog from "../lib/Dialog.vue"
 </script>
 
 <style lang="scss" scoped>
