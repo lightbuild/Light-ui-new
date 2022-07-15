d457cb0e8a5a4467fff6d617c79e5c1f051232f6
lightbuild <renran6303@gmail.com>
2022-07-15 17:31:20 +0800

    创建Tabs和Tab组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-    我是Tabs组件
-  </div>
+  <div>我是Tabs组件</div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script setup lang="ts">
+  import Tab from "../lib/Tab.vue"
+  import Tabs from "../lib/Tabs.vue"
 </script>
 
 <style lang="scss" scoped>
